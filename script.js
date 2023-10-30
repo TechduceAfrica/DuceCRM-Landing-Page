de2a0545630f09c05dc557c23208788e66a89e8a
Adekoye Adewale <adekoyeadewale@gmail.com>
2023-10-30 02:47:42 +0100

    Update
    Removed the mobile nav touch feature

--- script.js
+++ script.js
@@ -1,29 +1,27 @@
 const hamburger = document.querySelector(".mobile__nav");
 const mobileMenu = document.querySelector(".mobile__menu");
 
-let isClicked = false;
-let touchStartX = 0;
-let touchEndX = 0;
+// let isClicked = false;
+// let touchStartX = 0;
+// let touchEndX = 0;
 
-hamburger.addEventListener("touchstart", (event) => {
-    touchStartX = event.touches[0].clientX;
-});
+// hamburger.addEventListener("touchstart", (event) => {
+//     touchStartX = event.touches[0].clientX;
+// });
 
-hamburger.addEventListener("touchend", (event) => {
-    touchEndX = event.changedTouches[0].clientX;
+// hamburger.addEventListener("touchend", (event) => {
+//     touchEndX = event.changedTouches[0].clientX;
 
-    // Calculate the horizontal movement
-    const swipeDistance = touchEndX - touchStartX;
+//     const swipeDistance = touchEndX - touchStartX;
 
-    if (swipeDistance < 0) {
-        // Swipe left detected
-        if (!isClicked) {
-            hamburger.classList.add("clicked");
-            mobileMenu.classList.add("clicked");
-            isClicked = true;
-        }
-    }
-});
+//     if (swipeDistance < 0) {
+//         if (!isClicked) {
+//             hamburger.classList.add("clicked");
+//             mobileMenu.classList.add("clicked");
+//             isClicked = true;
+//         }
+//     }
+// });
 
 hamburger.addEventListener("click", () => {
     if (hamburger.classList.contains("clicked")) {
@@ -36,9 +34,7 @@ hamburger.addEventListener("click", () => {
 });
 
 window.addEventListener("click", (event) => {
-    // Check if the click target is not the mobile__nav or new-element
     if (event.target !== hamburger && event.target !== mobileMenu) {
-        // Remove the "clicked" class from both elements
         hamburger.classList.remove("clicked");
         mobileMenu.classList.remove("clicked");
     }
