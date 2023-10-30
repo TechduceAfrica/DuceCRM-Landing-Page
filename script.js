ef649d11d9a947590758cc36d901d448d841c2db
Adekoye Adewale <adekoyeadewale@gmail.com>
2023-10-30 11:03:04 +0100

    update
    Mobile nav bar bug fixed

--- script.js
+++ script.js
@@ -1,28 +1,6 @@
 const hamburger = document.querySelector(".mobile__nav");
 const mobileMenu = document.querySelector(".mobile__menu");
 
-// let isClicked = false;
-// let touchStartX = 0;
-// let touchEndX = 0;
-
-// hamburger.addEventListener("touchstart", (event) => {
-//     touchStartX = event.touches[0].clientX;
-// });
-
-// hamburger.addEventListener("touchend", (event) => {
-//     touchEndX = event.changedTouches[0].clientX;
-
-//     const swipeDistance = touchEndX - touchStartX;
-
-//     if (swipeDistance < 0) {
-//         if (!isClicked) {
-//             hamburger.classList.add("clicked");
-//             mobileMenu.classList.add("clicked");
-//             isClicked = true;
-//         }
-//     }
-// });
-
 hamburger.addEventListener("click", () => {
     if (hamburger.classList.contains("clicked")) {
         hamburger.classList.remove("clicked");
@@ -33,9 +11,9 @@ hamburger.addEventListener("click", () => {
     }
 });
 
-window.addEventListener("click", (event) => {
-    if (event.target !== hamburger && event.target !== mobileMenu) {
-        hamburger.classList.remove("clicked");
-        mobileMenu.classList.remove("clicked");
-    }
-});
+// window.addEventListener("click", (event) => {
+//     if (event.target !== hamburger && event.target !== mobileMenu) {
+//         hamburger.classList.remove("clicked");
+//         mobileMenu.classList.remove("clicked");
+//     }
+// });
