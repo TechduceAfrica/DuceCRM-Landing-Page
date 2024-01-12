208381db75b29b14523bc49788135554ffc078e4
Adekoye Adewale <adekoyeadewale@gmail.com>
2024-01-12 19:03:55 +0100

    Web copy upload

---
+++ ducecrm-v2/src/WebCopy/BenefitsCopy.js
@@ -0,0 +1,27 @@
+export const BenefitCopy = {
+    title: `All The Perks To Elevate Your Business`,
+    description: `We have prepared everything to aid your business goals and attain success`,
+}
+
+export const BenefitsCopy = [
+    {
+        title: `Efficient Communication`,
+        description: `Ensured speedy responses and enhanced satisfaction`,
+        img: `/images/Efficient%20communication%20-%20DureCRM.webp`,
+    },
+    {
+        title: `Improved Engagement`,
+        description: `Your customers get a more personalized experience`,
+        img: `/images/Improved%20Engagement%20-%20Duce%20CRM.webp`,
+    },
+    {
+        title: `Greater Efficiency`,
+        description: `Centralized customer information and automates task.`,
+        img: `/images/Greater%20Efficiency%20-%20DuceCRM.webp`,
+    },
+    {
+        title: `Optimizied Sale Strategy`,
+        description: `Simplified sales strategy with predictive revenue forecasting`,
+        img: `/images/OPtimized%20Sales%20Strategy%20-%20DuceCRM.webp`,
+    },
+]
