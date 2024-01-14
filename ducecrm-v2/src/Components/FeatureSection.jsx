e6a5011cf9d070ad64cfc58105de55591bd47d64
Adekoye Adewale <adekoyeadewale@gmail.com>
2024-01-14 23:57:54 +0100

    refactor: tag improvement for SEO structure

--- ducecrm-v2/src/Components/FeatureSection.jsx
+++ ducecrm-v2/src/Components/FeatureSection.jsx
@@ -3,9 +3,9 @@ import FeatureList from "./FeatureList";
 
 export default function FeatureSection() {
     return (
-        <div className="feature__sec container__width grid__j__items__cen">
+        <section className="feature__sec container__width">
             <h2 dangerouslySetInnerHTML={{ __html: FeatureCopy.title }} ></h2>
             <FeatureList/> 
-        </div>
+        </section>
     )
 }
