8c00b2a044f4b0473446a0facb35077bddba3189
Adekoye Adewale <adekoyeadewale@gmail.com>
2024-01-16 10:11:12 +0100

    refactor: added a new classname

--- ducecrm-v2/src/Components/Mainpage.jsx
+++ ducecrm-v2/src/Components/Mainpage.jsx
@@ -4,16 +4,20 @@ import HeroSection from './HeroSection'
 import FeatureSection from './FeatureSection'
 import Benefits from './Benefits'
 import VideoCtaSection from './VideoCtaSection'
+import FormSection from './FormSection'
+import FaqSection from './FaqSection'
 
 export default function Mainpage() {
     return (
         <>
             <NavBar/>
-            <main className='grid__j__items__cen'>
+            <main className='main__sec grid__j__items__cen'>
                 <HeroSection/>
                 <FeatureSection/>
                 <Benefits/>
                 <VideoCtaSection/>
+                <FormSection/>
+                <FaqSection/>
             </main>
         </>
     )
