d6da3ea845c5000fbe3f0588d02b68c97aa140a8
Adekoye Adewale <adekoyeadewale@gmail.com>
2024-01-16 20:55:30 +0100

    refcator: removed excess space

--- ducecrm-v2/src/WebCopy/HeroCopy.js
+++ ducecrm-v2/src/WebCopy/HeroCopy.js
@@ -1,6 +1,6 @@
 export const HeroCopy = {
     title: `Your Key To Better Customer Relationships!`,
-    description: `Transform Customer interactions and drive success with our all-in-one <span class="org">CRM </span>app`,
+    description: `Transform Customer interactions and drive success with our all-in-one<span className="org"> CRM </span>app`,
     link: `Join Our Waitlist`,
     img: `/images/DuceCRM%20-%20Sales%20Pipeline-Customer%20Overview.webp`,
     width: `525`,
